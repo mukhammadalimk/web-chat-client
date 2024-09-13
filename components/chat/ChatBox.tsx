c0931816a69a71977a0d108e3a23903135c6766b
Muhammadali Sulton <mukhammadalimk@gmail.com>
2024-09-13 13:04:21 +0900

    implemented sendMessageHandler

--- components/chat/ChatBox.tsx
+++ components/chat/ChatBox.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MessageItem from "./MessageItem";
+import axios from "axios";
 
 interface MessagesProps {
   text: string;
@@ -10,6 +11,45 @@ interface MessagesProps {
 const ChatBox = () => {
   const [messages, setMessages] = useState<MessagesProps[] | []>([]);
   const messageRef = useRef<HTMLInputElement>(null);
+
+  // Scroll to the bottom of the messages box when new message is created
+  useEffect(() => {
+    const chatBox = document.getElementById("messages") as HTMLDivElement;
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }, [messages]);
+
+  // Send message to backend
+  const sendMessageHandler = async () => {
+    // Get message text from messageRed
+    const messageText = messageRef.current?.value;
+
+    // Return if the messageText is empty
+    if (messageText === "") return;
+
+    // Add the message to the messages array
+    setMessages((prevState) => {
+      return [...prevState, { text: messageText!, start: false }];
+    });
+
+    // Clear the input
+    messageRef.current!.value = "";
+
+    try {
+      // Send request to backend
+      const { data } = await axios.post(
+        `https://web-chat-server-8hk1.onrender.com/chat`,
+        { message: messageText },
+        { withCredentials: true }
+      );
+
+      // Add the response message to the messages array
+      setMessages((prevState) => {
+        return [...prevState, { text: data.message, start: true }];
+      });
+    } catch (error) {
+      console.log("error:", error);
+    }
+  };
 
   return (
     <div className="w-full max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl h-[600px] md:h-[700px] bg-white rounded-lg shadow-lg flex flex-col">
@@ -28,7 +68,12 @@ const ChatBox = () => {
             className="input input-bordered join-item flex-grow"
             ref={messageRef}
           />
-          <button className="btn btn-primary join-item">Send</button>
+          <button
+            className="btn btn-primary join-item"
+            onClick={sendMessageHandler}
+          >
+            Send
+          </button>
         </div>
       </div>
     </div>
